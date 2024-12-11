8a6b1fbb979247b288931783b7fc10c1ebdf595f
irisHanb <fortrulymylife@gmail.com>
2024-12-11 19:38:55 +0900

    feat: SEO

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -1,6 +1,18 @@
 import { getMovie } from "actions/movieActions";
 import MovieDetailUI from "./ui";
 
+export async function generateMetadata({ params }) {
+  const movie = await getMovie(params.id);
+  const { title, overview, image_url } = movie;
+  return {
+    title,
+    description: overview,
+    openGraph: {
+      images: [image_url],
+    },
+  };
+}
+
 export default async function MovieDetailPage({ params }) {
   const movie = await getMovie(params.id);
 
