24f3d24e61d8be7da4a1092720dff7de7a755f4e
irisHanb <fortrulymylife@gmail.com>
2024-12-11 19:31:56 +0900

    feat: infinite scroll

--- actions/movieActions.ts
+++ actions/movieActions.ts
@@ -9,14 +9,31 @@ function handleError(error) {
   }
 }
 
-export async function searchMovies(search = "") {
+export async function searchMovies({ search = "", page, pageSize }) {
   const supabase = await createServerSupabaseClient();
-  const { data, error } = await supabase
+  const { data, count, error } = await supabase
     .from("movie")
     .select("*")
-    .like("title", `%${search}%`);
-  handleError(error);
-  return data;
+    .like("title", `%${search}%`)
+    .range((page - 1) * pageSize, page * pageSize - 1); // 시작 값, 끝나는 값
+
+  const hasNextPage = count > page * pageSize;
+
+  if (error) {
+    return {
+      data: [],
+      count: 0,
+      page: null,
+      pageSize: null,
+      error,
+    };
+  }
+  return {
+    data,
+    page,
+    pageSize,
+    hasNextPage,
+  };
 }
 
 export async function getMovie(id) {
--- components/movie-card-list.tsx
+++ components/movie-card-list.tsx
@@ -1,26 +1,52 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import MovieCard from "./movie-card";
 import { searchMovies } from "actions/movieActions";
 import { Spinner } from "@material-tailwind/react";
 import { useRecoilValue } from "recoil";
 import { searchState } from "utils/recoil/atoms";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
 export default function MovieCardList() {
   const search = useRecoilValue(searchState);
-  console.log(search);
-  const searchMoviesQuery = useQuery({
-    queryKey: ["movie", search],
-    queryFn: () => searchMovies(search),
+
+  const { data, isFetching, isFetchingNextPage, fetchNextPage, hasNextPage } =
+    useInfiniteQuery({
+      initialPageParam: 1,
+      queryKey: ["movie", search],
+      queryFn: ({ pageParam }) =>
+        searchMovies({ search, page: pageParam, pageSize: 12 }),
+      getNextPageParam: (lastPage) =>
+        lastPage.page ? lastPage.page + 1 : null,
+    });
+
+  const { ref, inView } = useInView({
+    threshold: 0,
   });
+
+  useEffect(() => {
+    console.log(inView);
+    if (inView && hasNextPage && !isFetching && !isFetchingNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage]);
+
   return (
     <div className="grid gap-1 grid-cols-3 md:grid-cols-4  w-full h-full p-4">
-      {searchMoviesQuery.isLoading && <Spinner />}
-      {searchMoviesQuery.data &&
-        searchMoviesQuery.data.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
+      {(isFetching || isFetchingNextPage) && <Spinner />}
+      {
+        <>
+          {data?.pages
+            ?.map((page) => page.data)
+            ?.flat()
+            .map((movie) => (
+              <MovieCard key={movie.id} movie={movie} />
+            ))}
+          <div ref={ref}></div>
+        </>
+      }
     </div>
   );
 }
